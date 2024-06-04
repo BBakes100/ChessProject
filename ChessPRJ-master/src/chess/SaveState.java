8f74f3d165eeed2fb5c6a6c907d90ecd516ad17c
Brandon Baker <bakebran@mail.gvsu.edu>
2024-06-04 13:23:17 -0400

    initial files

---
+++ ChessPRJ-master/src/chess/SaveState.java
@@ -0,0 +1,10 @@
+package chess;
+
+public class SaveState {
+    public int saveFromRow;
+    public int saveToRow;
+    public int saveFromCol;
+    public int saveToCol;
+    public IChessPiece saveFromPiece;
+    public IChessPiece saveToPiece;
+}
